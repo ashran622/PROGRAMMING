a53a72e356169d13d38d6f672e786a0541aa9081
RandyVega <41728169+ashran622@users.noreply.github.com>
2018-08-28 11:26:33 +0800

    Add files via upload

---
+++ PROG_EXER3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+	string name, birthdate, email, phone, address;
+	{
+		cout << "\nName: ";
+		cin >> name;
+		cout << "\nBirthdate: ";
+		cin >> birthdate;
+		cout <<"\nEmail: ";
+		cin >> email;
+		cout <<"\nPhone: ";
+		cin >> phone;
+		cout << "\nAddress: ";
+		cin >> address;
+		
+		return 0;
+		
+	}
+}
